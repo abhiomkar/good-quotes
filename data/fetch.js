dfc72dd127fc1e86cf3d5bc703e9f284556744e6
Abhinay Omkar <abhiomkar@gmail.com>
2023-10-21 18:48:38 +0530

    Cleanup fetch.js and store the updated data

--- data/fetch.js
+++ data/fetch.js
@@ -1,25 +1,46 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
-const fs = require('fs');
+/**
+ * Fetches quotes from the Goodreads website.
+ * @url https://www.goodreads.com/quotes
+ */
+
+const axios = require("axios");
+const cheerio = require("cheerio");
+const fs = require("fs");
+const path = require("path");
 
 async function scrapeHTMLPages(url, numPages) {
   const quotes = [];
 
   for (let i = 1; i <= numPages; i++) {
     const pageUrl = `${url}?page=${i}`;
+    console.log("Fetching page: ", pageUrl);
     const response = await axios.get(pageUrl);
     const html = response.data;
     const $ = cheerio.load(html);
 
     // Extract and process quotes from the HTML
     // Add them to the 'quotes' array
-    $('.quoteText').each((index, element) => {
-      const quote = $(element).contents().filter(function() {
-        return this.nodeType === 3; // Node.TEXT_NODE
-      }).first().text().trim().replace(/^“/, '').replace(/”$/, '');
-      const authorOrTitleQuery = $(element).find('.authorOrTitle');
-      const author = $(authorOrTitleQuery).first().text().trim().replace(/,$/, '');
-      const title = (authorOrTitleQuery.length === 2) ? $(authorOrTitleQuery).last().text().trim() : '';
+    $(".quoteText").each((index, element) => {
+      const quote = $(element)
+        .contents()
+        .filter(function () {
+          return this.nodeType === 3; // Node.TEXT_NODE
+        })
+        .first()
+        .text()
+        .trim()
+        .replace(/^“/, "")
+        .replace(/”$/, "");
+      const authorOrTitleQuery = $(element).find(".authorOrTitle");
+      const author = $(authorOrTitleQuery)
+        .first()
+        .text()
+        .trim()
+        .replace(/,$/, "");
+      const title =
+        authorOrTitleQuery.length === 2
+          ? $(authorOrTitleQuery).last().text().trim()
+          : "";
 
       quotes.push({
         quote,
@@ -28,8 +49,10 @@ async function scrapeHTMLPages(url, numPages) {
       });
     });
 
-    // Wait to prevent timeout errors.    
-    await new Promise(r => setTimeout(r, getRandomDelay()));
+    console.log("Found", $(".quoteText").length, "quotes on page", i);
+
+    // Wait to prevent timeout errors.
+    await new Promise((r) => setTimeout(r, getRandomDelay()));
   }
 
   return quotes;
@@ -41,14 +64,17 @@ function getRandomDelay() {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-const url = 'https://www.goodreads.com/quotes';
+const url = "https://www.goodreads.com/quotes";
 const numPages = 100;
 
 scrapeHTMLPages(url, numPages)
-  .then(quotes => {
+  .then((quotes) => {
     console.log(quotes);
-    fs.writeFileSync('./goodquotes.json', JSON.stringify(quotes, null, 2));
+    fs.writeFileSync(
+      path.join(__dirname, "/goodquotes.json"),
+      JSON.stringify(quotes, null, 2)
+    );
   })
-  .catch(error => {
+  .catch((error) => {
     console.log(error);
   });
