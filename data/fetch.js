cd2a59ae50ef7b7d8b6b7137ac0dec886a9a2fb8
Abhinay Omkar <abhiomkar@gmail.com>
2023-10-21 18:29:26 +0530

    Add script to scrape good quotes

---
+++ data/fetch.js
@@ -0,0 +1,54 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+const fs = require('fs');
+
+async function scrapeHTMLPages(url, numPages) {
+  const quotes = [];
+
+  for (let i = 1; i <= numPages; i++) {
+    const pageUrl = `${url}?page=${i}`;
+    const response = await axios.get(pageUrl);
+    const html = response.data;
+    const $ = cheerio.load(html);
+
+    // Extract and process quotes from the HTML
+    // Add them to the 'quotes' array
+    $('.quoteText').each((index, element) => {
+      const quote = $(element).contents().filter(function() {
+        return this.nodeType === 3; // Node.TEXT_NODE
+      }).first().text().trim().replace(/^“/, '').replace(/”$/, '');
+      const authorOrTitleQuery = $(element).find('.authorOrTitle');
+      const author = $(authorOrTitleQuery).first().text().trim().replace(/,$/, '');
+      const title = (authorOrTitleQuery.length === 2) ? $(authorOrTitleQuery).last().text().trim() : '';
+
+      quotes.push({
+        quote,
+        author,
+        title,
+      });
+    });
+
+    // Wait to prevent timeout errors.    
+    await new Promise(r => setTimeout(r, getRandomDelay()));
+  }
+
+  return quotes;
+}
+
+function getRandomDelay() {
+  const min = 1500;
+  const max = 2500;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const url = 'https://www.goodreads.com/quotes';
+const numPages = 100;
+
+scrapeHTMLPages(url, numPages)
+  .then(quotes => {
+    console.log(quotes);
+    fs.writeFileSync('./goodquotes.json', JSON.stringify(quotes, null, 2));
+  })
+  .catch(error => {
+    console.log(error);
+  });
